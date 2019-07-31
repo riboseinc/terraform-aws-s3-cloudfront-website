092046081bf1561996142371d4cbaaf19cf62b19
phuong <phuonghqh@gmail.com>
2019-07-31 13:08:14 +0700

    Comment out providers section

--- provider.tf
+++ provider.tf
@@ -1,15 +1,14 @@
 # Terraform doesn't allow specifying required providers yet, but this
 # is placed here in case it is allowed:
 # https://github.com/hashicorp/terraform/issues/17191
-provider "aws" {
-  alias = "main"
-  version = "~> 2.9"
-}
-
-#
-provider "aws" {
-  alias = "cloudfront"
-  version = "~> 2.9"
-}
+//provider "aws" {
+//  alias = "main"
+//  version = "~> 2.9"
+//}
+//
+//provider "aws" {
+//  alias = "cloudfront"
+//  version = "~> 2.9"
+//}
 
 
