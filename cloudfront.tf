1e89eff08faa6806acc813790b7803701f51863a
Mike Keen <mike@stavvy.com>
2020-06-23 11:51:43 -0400

    bust cache

--- cloudfront.tf
+++ cloudfront.tf
@@ -80,6 +80,26 @@ resource "aws_cloudfront_distribution" "main" {
     max_ttl                = 1200
   }
 
+  ordered_cache_behavior {
+    target_origin_id = "origin-${var.fqdn}"
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    path_pattern = "index.html"
+    
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl = 0
+    default_ttl = 0
+    max_ttl = 0
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
