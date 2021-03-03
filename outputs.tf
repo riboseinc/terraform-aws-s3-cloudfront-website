88e86c80c87cd3d58340af3167168b728d60d2fb
phuong <phuonghqh@gmail.com>
2021-03-04 02:02:35 +0700

    Use v12 syntax

--- outputs.tf
+++ outputs.tf
@@ -19,21 +19,21 @@ output "s3_hosted_zone_id" {
 
 output "cf_domain_name" {
   value = concat(
-      aws_cloudfront_distribution.main-lambda-edge.*.domain_name,
+//      aws_cloudfront_distribution.main-lambda-edge.*.domain_name,
       aws_cloudfront_distribution.main.*.domain_name,
     )[0]
 }
 
 output "cf_hosted_zone_id" {
   value = concat(
-      aws_cloudfront_distribution.main-lambda-edge.*.hosted_zone_id,
+//      aws_cloudfront_distribution.main-lambda-edge.*.hosted_zone_id,
       aws_cloudfront_distribution.main.*.hosted_zone_id,
     )[0]
 }
 
 output "cf_distribution_id" {
   value = concat(
-      aws_cloudfront_distribution.main-lambda-edge.*.id,
+//      aws_cloudfront_distribution.main-lambda-edge.*.id,
       aws_cloudfront_distribution.main.*.id,
     )[0]
 }
--- variables.tf
+++ variables.tf
@@ -1,101 +1,101 @@
 variable "fqdn" {
-  type        = string
+  type = string
   description = "The FQDN of the website and also name of the S3 bucket"
 }
 
 variable "aliases" {
-  type        = list(string)
+  type = list(string)
   description = "Any other domain aliases to add to the CloudFront distribution"
-  default     = []
+  default = []
 }
 
 variable "acceleration_status" {
-  type        = string
+  type = string
   description = "The acceleration_status argument of the S3 bucket (warning: this is not possible right now for hosted sites, as bucket names cannot contain a dot.)"
-  default     = "Suspended"
+  default = "Suspended"
 }
 
 variable "force_destroy" {
-  type        = string
+  type = string
   description = "The force_destroy argument of the S3 bucket"
-  default     = "false"
+  default = "false"
 }
 
 variable "ssl_certificate_arn" {
-  type        = string
+  type = string
   description = "ARN of the certificate covering the fqdn and its apex?"
 }
 
 variable "allowed_ips" {
-  type        = list(string)
+  type = list(string)
   description = "A list of IPs that can access the S3 bucket directly"
-  default     = []
+  default = []
 }
 
 variable "web_acl_id" {
-  type        = string
+  type = string
   description = "WAF Web ACL ID to attach to the CloudFront distribution, optional"
-  default     = ""
+  default = ""
 }
 
 variable "refer_secret" {
-  type        = string
+  type = string
   description = "A secret string to authenticate CF requests to S3"
-  default     = "123-VERY-SECRET-123"
+  default = "123-VERY-SECRET-123"
 }
 
 variable "routing_rules" {
-  type        = string
+  type = string
   description = "Routing rules for the S3 bucket"
-  default     = ""
+  default = ""
 }
 
 variable "cloudfront_price_class" {
-  type        = string
+  type = string
   description = "PriceClass for CloudFront distribution"
-  default     = "PriceClass_100"
+  default = "PriceClass_100"
 }
 
 variable "index_document" {
-  type        = string
+  type = string
   description = "HTML to show at root"
-  default     = "index.html"
+  default = "index.html"
 }
 
 variable "error_document" {
-  type        = string
+  type = string
   description = "HTML to show on 404"
-  default     = "404.html"
+  default = "404.html"
 }
 
 variable "error_response_code" {
-  type        = string
+  type = string
   description = "Response code to send on 404"
-  default     = "404"
+  default = "404"
 }
 
 variable "spa_error_response_code" {
-  type        = string
+  type = string
   description = "Response code to send on 404 for a single page application"
-  default     = "200"
+  default = "200"
 }
 
 variable "tags" {
-  type        = map(string)
+  type = map(string)
   description = "Tags"
-  default     = {}
+  default = {}
 }
 
 # value should be  "${lambda.arn}:${lambda.version}"
-variable "lambda_edge_arn_version" {
-  default = ""
-  type = string
-}
-
-variable "lambda_edge_enabled" {
-  default = false
-  type = bool
-}
+//variable "lambda_edge_arn_version" {
+//  default = ""
+//  type = string
+//}
+//
+//variable "lambda_edge_enabled" {
+//  default = false
+//  type = bool
+//}
 
 variable "cf_ipv6_enabled" {
   default = true
@@ -105,4 +105,13 @@ variable "cf_ipv6_enabled" {
 variable "single_page_application" {
   default = false
   type = bool
+}
+
+variable "lambda_edges" {
+  default = []
+
+  type = list(object({
+    event_type = string
+    lambda_arn = string
+  }))
 }
