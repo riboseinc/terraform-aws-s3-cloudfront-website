985265e0a5ac9e1019cb8b41dbefcfc23282a3e4
phuong <phuonghqh@gmail.com>
2021-06-03 13:44:01 +0700

    Require aws v3.x

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 2.9"
+      version = "~> 3.0"
       configuration_aliases = [aws.main, aws.cloudfront]
     }
   }
