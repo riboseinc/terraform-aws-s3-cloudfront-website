6392793a0bd8c42b7dafa85b3b21367a061c6f87
kate <kate.ruh@gmail.com>
2021-07-12 12:07:05 -0400

    Encryption at rest

--- s3.tf
+++ s3.tf
@@ -4,6 +4,15 @@ resource "aws_s3_bucket" "main" {
   acl      = "private"
   policy   = data.aws_iam_policy_document.bucket_policy.json
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
   website {
     index_document = var.index_document
     routing_rules  = var.routing_rules
@@ -17,6 +26,11 @@ resource "aws_s3_bucket" "main" {
       "Name" = var.fqdn
     },
   )
+}
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
 }
 
 data "aws_iam_policy_document" "bucket_policy" {
