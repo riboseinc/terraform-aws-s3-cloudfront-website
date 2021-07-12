81812bb3d53fe7b44d92accb3ccfc8ebb658a99a
kate <kate.ruh@gmail.com>
2021-07-12 12:23:05 -0400

    Add versioning, quick fix

--- s3.tf
+++ s3.tf
@@ -18,13 +18,8 @@ resource "aws_s3_bucket" "main" {
     }
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.mykey.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
+  versioning {
+    enabled = true
   }
 
   website {
