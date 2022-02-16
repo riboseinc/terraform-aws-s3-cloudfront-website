8f97ecf580fe292e5971aca7d5ee397ca95213b4
phuong <phuonghqh@gmail.com>
2022-02-17 01:42:38 +0700

    Upgrade to aws 4x - not completed

--- sample-site/acm.tf
+++ sample-site/acm.tf
@@ -43,7 +43,7 @@ resource "aws_acm_certificate_validation" "cert" {
 
 data "aws_route53_zone" "main" {
   provider     = aws.main
-  name         = "booppi.website"
+  name         = "kamehameha.site"
   private_zone = false
 }
 
--- sample-site/providers.tf
+++ sample-site/providers.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   alias = "main"
   region = "us-east-1"
+#  version = "~>4.0"
 }
 
 provider "aws" {
