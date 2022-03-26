200d77ef36933d7962c7526f3dd7ffffe2500c3b
phuong <phuonghqh@gmail.com>
2022-03-26 16:11:04 +0700

    Adding routing_rule

--- variables.tf
+++ variables.tf
@@ -1,116 +1,122 @@
 variable "fqdn" {
-  type = string
+  type        = string
   description = "The FQDN of the website and also name of the S3 bucket"
 }
 
 variable "bucket_name" {
-  type = string
+  type        = string
   description = <<EOF
     To control name of the bucket of the website, default is "{var.fqdn}"
     Warning: to use `acceleration_status` mode, the name can not contains Dots(.)
   EOF
-//  description = "(var.fqdn is used if value is null) name of the buckets for the site, should not contain Dots(.) if acceleration_status != null"
-  default = null
+  //  description = "(var.fqdn is used if value is null) name of the buckets for the site, should not contain Dots(.) if acceleration_status != null"
+  default     = null
 }
 
 variable "aliases" {
-  type = list(string)
+  type        = list(string)
   description = "Any other domain aliases to add to the CloudFront distribution"
-  default = []
+  default     = []
 }
 
 variable "acceleration_status" {
-  type = string
+  type        = string
   description = <<EOF
     Possible values: (Suspended | Enabled | null)
     The acceleration_status argument of the S3 bucket
     Warning: current Bucket might be destroyed, as its name cannot contain a dot.
     Variable `bucket_name` might be used to control this behavior.
   EOF
-//  description = "(Suspended | Enabled | null) The acceleration_status argument of the S3 bucket (warning: current Bucket might be destroyed, as its name cannot contain a dot)"
-  default = null
+  //  description = "(Suspended | Enabled | null) The acceleration_status argument of the S3 bucket (warning: current Bucket might be destroyed, as its name cannot contain a dot)"
+  default     = null
+  nullable    = true
+
+  validation {
+    condition     = var.acceleration_status == null || var.acceleration_status == "Suspended" || var.acceleration_status == "Enabled"
+    error_message = "Valid values should be 'Suspended' or 'Enabled'."
+  }
 }
 
 variable "force_destroy" {
-  type = string
+  type        = string
   description = "The force_destroy argument of the S3 bucket"
-  default = "false"
+  default     = "false"
 }
 
 variable "ssl_certificate_arn" {
-  type = string
+  type        = string
   description = "ARN of the certificate covering the fqdn and its apex?"
 }
 
 variable "allowed_ips" {
-  type = list(string)
+  type        = list(string)
   description = "A list of IPs that can access the S3 bucket directly"
-  default = []
+  default     = []
 }
 
 variable "web_acl_id" {
-  type = string
+  type        = string
   description = "WAF Web ACL ID to attach to the CloudFront distribution, optional"
-  default = ""
+  default     = ""
 }
 
 variable "refer_secret" {
-  type = string
+  type        = string
   description = "A secret string to authenticate CF requests to S3"
-  default = "123-VERY-SECRET-123"
+  default     = "123-VERY-SECRET-123"
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration#routing_rule
 variable "routing_rules" {
-  type = map
+  type        = map
   description = "Routing rules for the S3 bucket"
-  default = {}
+  default     = {}
 }
 
 variable "cloudfront_price_class" {
-  type = string
+  type        = string
   description = "PriceClass for CloudFront distribution"
-  default = "PriceClass_100"
+  default     = "PriceClass_100"
 }
 
 variable "index_document" {
-  type = string
+  type        = string
   description = "HTML to show at root"
-  default = "index.html"
+  default     = "index.html"
 }
 
 variable "error_document" {
-  type = string
+  type        = string
   description = "HTML to show on 404"
-  default = "404.html"
+  default     = "404.html"
 }
 
 variable "error_response_code" {
-  type = string
+  type        = string
   description = "Response code to send on 404"
-  default = "404"
+  default     = "404"
 }
 
 variable "spa_error_response_code" {
-  type = string
+  type        = string
   description = "Response code to send on 404 for a single page application"
-  default = "200"
+  default     = "200"
 }
 
 variable "tags" {
-  type = map(string)
+  type        = map(string)
   description = "Tags"
-  default = {}
+  default     = {}
 }
 
 variable "cf_ipv6_enabled" {
   default = true
-  type = bool
+  type    = bool
 }
 
 variable "single_page_application" {
   default = false
-  type = bool
+  type    = bool
 }
 
 variable "lambda_edges" {
@@ -121,3 +127,21 @@ variable "lambda_edges" {
     lambda_arn = string #"${lambda.arn}:${lambda.version}"
   }))
 }
+
+variable "test_var" {
+  type = string
+  default = null
+}
+
+#"RoutingRules": [
+#  {
+#    "RoutingRuleCondition": {
+#      "HttpErrorCodeReturnedEquals": "404",
+#      "KeyPrefixEquals": "out1/"
+#    },
+#    "RedirectRule": {
+#      "HostName": "ec2-11-22-333-44.compute-1.amazonaws.com",
+#      "ReplaceKeyPrefixWith": "report-404/"
+#    }
+#  }
+#]
